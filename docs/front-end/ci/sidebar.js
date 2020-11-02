703fbe1beb438729a978e07c0710df8f64c8b73d
酵父 <fred26s@163.com>
2020-11-02 11:23:39 +0800

    尝试travis服务器自动构建

--- docs/front-end/ci/sidebar.js
+++ docs/front-end/ci/sidebar.js
@@ -2,6 +2,9 @@ module.exports = [
   {
     title: "前端自动化",
     collapsable: false,
-    children: [{ title: "travis自动化部署", path: "/front-end/ci/travis自动部署" }],
+    children: [
+      { title: "travis自动化部署", path: "/front-end/ci/travis自动部署" },
+      { title: "前端自动化工具对比", path: "/front-end/ci/前端自动化工具对比" },
+    ],
   },
 ];
---
+++ travis_init.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# 切换源
+npm set registry https://registry.npm.taobao.org/
+
+# 全局安装 vuepress
+npm install -g vuepress
