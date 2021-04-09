f5c95be9b5ff1030fa5eb20e849eb1a7215a1db4
石飞帆 <shifeifan@e6yun.com>
2021-04-09 18:20:56 +0800

    增加稳定币.md

--- docs/funs/crypt-currency/sidebar.js
+++ docs/funs/crypt-currency/sidebar.js
@@ -5,7 +5,8 @@ module.exports = [
     children: [
       { title: "eth", path: "/funs/crypt-currency/eth.md" },
       { title: "eth扩容", path: "/funs/crypt-currency/eth扩容.md" },
-      { title: "defi", path: "/funs/crypt-currency/defi.md" }
+      { title: "defi", path: "/funs/crypt-currency/defi.md" },
+      { title: "稳定币", path: "/funs/crypt-currency/稳定币.md" }
     ],
   },
   {
