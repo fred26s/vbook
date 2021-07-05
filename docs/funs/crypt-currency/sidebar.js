7847a56e3a74b413f3a65e477e63db65a1eef2cf
酵父 <fred26s@163.com>
2021-07-05 22:52:20 +0800

    增加灰度.md

--- docs/funs/crypt-currency/sidebar.js
+++ docs/funs/crypt-currency/sidebar.js
@@ -9,7 +9,7 @@ module.exports = [
       { title: "稳定币", path: "/funs/crypt-currency/稳定币.md" },
       { title: "solana", path: "/funs/crypt-currency/SOL.md" },
       { title: "terra", path: "/funs/crypt-currency/terra.md" },
-      { title: "数字钱包", path: "/funs/crypt-currency/数字钱包.md" }
+      { title: "数字钱包", path: "/funs/crypt-currency/数字钱包.md" },
     ],
   },
   {
@@ -17,6 +17,7 @@ module.exports = [
     collapsable: true,
     children: [
       { title: "btc", path: "/funs/crypt-currency/btc.md" },
+      { title: "灰度", path: "/funs/crypt-currency/灰度.md" },
     ],
-  }
+  },
 ];
