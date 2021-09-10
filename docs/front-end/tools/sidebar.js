68c5d322b6cddb05cdfdd44ac3cdffd027455e91
石飞帆 <shifeifan@e6yun.com>
2021-09-10 18:33:23 +0800

    GIT版本分支管理.md

--- docs/front-end/tools/sidebar.js
+++ docs/front-end/tools/sidebar.js
@@ -4,6 +4,7 @@ module.exports = [
     collapsable: false,
     children: [
       { title: "GIT", path: "/front-end/tools/GIT" },
+      { title: "GIT版本管理", path: "/front-end/tools/GIT版本分支管理" },
       { title: "HEXO", path: "/front-end/tools/HEXO" },
       { title: "Fiddler抓包", path: "/front-end/tools/Fiddler" },
       { title: "vscode", path: "/front-end/tools/vscode" },
