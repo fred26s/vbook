fd1e8046cafed4bc487ee3aa66bb562557bfde4c
石飞帆 <shifeifan@e6yun.com>
2021-09-17 18:27:47 +0800

    技巧函数.md

--- docs/front-end/js/sidebar.js
+++ docs/front-end/js/sidebar.js
@@ -2,6 +2,9 @@ module.exports = [
   {
     title: "JS",
     collapsable: false,
-    children: [{ title: "ES6", path: "/front-end/js/es6知识" }],
+    children: [
+      { title: "ES6", path: "/front-end/js/es6知识" },
+      { title: "技巧函数", path: "/front-end/js/技巧函数" },
+    ],
   },
 ];
