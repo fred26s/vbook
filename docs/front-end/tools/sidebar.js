e7d5a1f542481da66f9497f2e7b0dd8f8008a92f
石飞帆 <shifeifan@e6yun.com>
2021-09-18 17:48:06 +0800

    增加windows-dos.md

--- docs/front-end/tools/sidebar.js
+++ docs/front-end/tools/sidebar.js
@@ -1,13 +1,20 @@
 module.exports = [
   {
     title: "开发工具",
-    collapsable: false,
+    collapsable: true,
     children: [
       { title: "GIT", path: "/front-end/tools/GIT" },
       { title: "GIT版本管理", path: "/front-end/tools/GIT版本分支管理" },
       { title: "HEXO", path: "/front-end/tools/HEXO" },
       { title: "Fiddler抓包", path: "/front-end/tools/Fiddler" },
       { title: "vscode", path: "/front-end/tools/vscode" },
+    ],
+  },
+  {
+    title: "命令行",
+    collapsable: true,
+    children: [
+      { title: "DOS", path: "/front-end/tools/windows-dos" },
     ],
   },
 ];
