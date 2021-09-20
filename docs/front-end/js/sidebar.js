7c25f86eb62d13d83200c7cb5bd0dc342d9152a8
酵父 <fred26s@163.com>
2021-09-20 21:01:53 +0800

    算法.md

--- docs/front-end/js/sidebar.js
+++ docs/front-end/js/sidebar.js
@@ -1,10 +1,15 @@
 module.exports = [
   {
-    title: "JS",
-    collapsable: false,
+    title: "JS知识",
+    collapsable: true,
     children: [
       { title: "ES6", path: "/front-end/js/es6知识" },
       { title: "技巧函数", path: "/front-end/js/技巧函数" },
     ],
+  },
+  {
+    title: "算法",
+    collapsable: true,
+    children: [{ title: "JS算法", path: "/front-end/js/算法" }],
   },
 ];
