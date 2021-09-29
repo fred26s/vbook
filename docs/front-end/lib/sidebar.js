00a2d55a61e9c5c901017950eeaf1e8b190072e7
jf <fred26s@163.com>
2021-09-29 17:49:49 +0800

    地图坐标系.md

--- docs/front-end/lib/sidebar.js
+++ docs/front-end/lib/sidebar.js
@@ -2,6 +2,9 @@ module.exports = [
   {
     title: "前端类库",
     collapsable: false,
-    children: [{ title: "echarts", path: "/front-end/lib/echarts" }],
+    children: [
+      { title: "echarts", path: "/front-end/lib/echarts" },
+      { title: "地图坐标系", path: "/front-end/lib/gcoord" },
+    ],
   },
 ];
