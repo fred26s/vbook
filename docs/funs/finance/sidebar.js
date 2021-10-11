f619b7eb602d6a592b38d48c2872c609009eed5d
jf <fred26s@163.com>
2021-10-11 18:01:24 +0800

    基金.md

--- docs/funs/finance/sidebar.js
+++ docs/funs/finance/sidebar.js
@@ -4,6 +4,7 @@ module.exports = [
     collapsable: false,
     children: [
       { title: "经济基本概念", path: "/funs/finance/经济学.md" },
+      { title: "基金/ETF/信托", path: "/funs/finance/基金-etf-信托.md" },
     ],
   },
 ];
