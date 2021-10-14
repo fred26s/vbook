3c1bdcf7ce8e6a4beeb25ffb2acc380a200f2b0c
jf <fred26s@163.com>
2021-10-14 18:13:40 +0800

    vogue.md

--- docs/funs/store/sidebar.js
+++ docs/funs/store/sidebar.js
@@ -4,6 +4,7 @@ module.exports = [
     collapsable: false,
     children: [
       { title: "备忘", path: "/funs/store/remarks.md" },
+      { title: "新概念", path: "/funs/store/vogue.md" },
     ],
   },
 ];
