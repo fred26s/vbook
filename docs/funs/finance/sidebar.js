8080f20bab2fd2482c5208162ffa7afe4bcc2a99
jf <fred26s@163.com>
2021-10-19 17:49:21 +0800

    人民币汇率 两融

--- docs/funs/finance/sidebar.js
+++ docs/funs/finance/sidebar.js
@@ -5,6 +5,7 @@ module.exports = [
     children: [
       { title: "经济基本概念", path: "/funs/finance/经济学.md" },
       { title: "基金/ETF/信托", path: "/funs/finance/基金-etf-信托.md" },
+      { title: "A股", path: "/funs/finance/A股.md" },
     ],
   },
 ];
