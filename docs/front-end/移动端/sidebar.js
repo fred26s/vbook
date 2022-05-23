be239b126242cfbc3d0069ab25ed9ba3a5483f58
jf <fred26s@163.com>
2022-05-23 18:36:13 +0800

    微信公众号.md

--- docs/front-end/移动端/sidebar.js
+++ docs/front-end/移动端/sidebar.js
@@ -7,6 +7,9 @@ module.exports = [
   {
     title: "wx生态开发",
     collapsable: true,
-    children: [{ title: "微信小程序", path: "/front-end/移动端/wx-miniprogram" }],
+    children: [
+	  { title: "微信小程序", path: "/front-end/移动端/wx-miniprogram" },
+	  { title: "公众号", path: "/front-end/移动端/微信公众号" }
+	],
   },
 ];
