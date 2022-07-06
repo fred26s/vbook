4e6f628e7811776247d2c257ee76d32c0e0bf070
jf <fred26s@163.com>
2022-07-06 20:39:11 +0800

    图片源类型.md

--- docs/front-end/net/sidebar.js
+++ docs/front-end/net/sidebar.js
@@ -2,6 +2,10 @@ module.exports = [
   {
     title: "网络基础",
     collapsable: false,
-    children: [{ title: "网络基础", path: "/front-end/net/网络基础" }],
+    children: [
+      { title: "网络基础", path: "/front-end/net/网络基础" },
+      { title: "影音相关", path: "/front-end/net/影音相关" },
+      { title: "图片相关", path: "/front-end/net/图片" },
+    ],
   },
 ];
