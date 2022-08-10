98b216f28e78812f56b3063da8cbd0060ab6110e
jf <fred26s@163.com>
2022-08-10 18:31:40 +0800

    优化.md

--- docs/front-end/evo/sidebar.js
+++ docs/front-end/evo/sidebar.js
@@ -3,7 +3,8 @@ module.exports = [
     title: "前端优化",
     collapsable: false,
     children: [
-      { title: "浏览器", path: "/front-end/evo/优化方式" },
+      { title: "浏览器", path: "/front-end/evo/浏览器" },
+      { title: "优化方式", path: "/front-end/evo/优化方式" },
       { title: "gulp优化压缩", path: "/front-end/evo/gulp优化" },
       { title: "CDN加速", path: "/front-end/evo/cdn" },
       { title: "浏览器缓存", path: "/front-end/evo/浏览器缓存" },
