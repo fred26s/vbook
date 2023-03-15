68037d90f5f834243b37d41ed1da04b6f326c45b
酵父 <fred26s@163.com>
2023-03-15 08:14:40 +0800

    资产负债表.md 美联储.md

--- docs/funs/finance/sidebar.js
+++ docs/funs/finance/sidebar.js
@@ -4,6 +4,7 @@ module.exports = [
     collapsable: false,
     children: [
       { title: "经济基本概念", path: "/funs/finance/经济学.md" },
+      { title: "美联储", path: "/funs/finance/fed.md" },
       { title: "基金/ETF/信托", path: "/funs/finance/基金-etf-信托.md" },
       { title: "A股", path: "/funs/finance/A股.md" },
       { title: "CFD", path: "/funs/finance/CFD.md" },
